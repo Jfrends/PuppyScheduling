f3b18402e78c41e7c737ca026d410aae6eb30e3e
Jfrends <jfrends10@gmail.com>
2026-09-02 20:17:29 -0700

    fixed router

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter basename="/PuppyScheduling">
+    // Replaced BrowserRouter with HashRouter and removed the basename
+    <HashRouter>
       <Navbar />
       <Routes>
         {/* All other routes */}
         <Route path="/*" element={<AppRoutes />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
